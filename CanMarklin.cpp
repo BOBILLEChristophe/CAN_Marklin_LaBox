481a73c46dc79a448e5aa1cea1669fcba9e76192
BOBILLEChristophe <38000184+BOBILLEChristophe@users.noreply.github.com>
2026-06-10 17:13:14 -0500

    Add files via upload

---
+++ CanMarklin.cpp
@@ -0,0 +1,328 @@
+/*
+
+  CanMsg.cpp
+
+ */
+
+#include "CanMarklin.h"
+
+#ifdef ENABLE_CANMARKLIN
+
+gpio_num_t CanMarklin::RxPin = GPIO_NUM_4;
+gpio_num_t CanMarklin::TxPin = GPIO_NUM_5;
+uint32_t CanMarklin::DESIRED_BIT_RATE = 1000ul * 1000ul;
+uint16_t CanMarklin::thisId = 0x1810;
+bool CanMarklin::SendCommands = false;
+
+uint16_t CanMarklin::getId() { return thisId; }
+
+QueueHandle_t xQueue; // Queue pour stocker les messages retour
+
+// Utilisation d'un unordered_map pour stocker les locomotives en fonction de leur adresse
+std::unordered_map<uint32_t, CanMarklinLoco> CanMarklinLoco::locoMap;
+// Recherche d'une locomotive par son adresse
+CanMarklinLoco *CanMarklinLoco::findLoco(uint32_t address)
+{
+  auto it = locoMap.find(address); // Rechercher si la locomotive existe déjà dans la map
+  if (it != locoMap.end())
+    return &it->second; // Si on la trouve, retourne un pointeur vers l'objet existant
+  // Si elle n'existe pas, on la crée et on l'ajoute à la map
+  locoMap[address] = CanMarklinLoco(address); // Insertion directe dans la map
+  return &locoMap[address];                   // Retourne la référence à l'objet créé
+}
+
+void ackTask(void *pvParameters)
+{
+  BaseType_t status;
+  CANMessage frame;
+
+  for (;;)
+  {
+    status = xQueueReceive(xQueue, &frame, portMAX_DELAY);
+    if (status == pdPASS)
+    {
+      frame.id &= (uint32_t)~0xFFFF; // Efface les 16 bits de poids faible
+      frame.id |= (uint32_t)1 << 16; // Réponse
+      // frame.id |= (uint32_t)0x1810; // ID expediteur
+      frame.id |= (uint32_t)CanMarklin::getId(); // ID expediteur
+      CanMarklin::sendMsg(frame);
+    }
+    vTaskDelay(pdMS_TO_TICKS(10));
+  }
+}
+
+void CanMarklin::begin()
+{
+  DIAG(F("Configure ESP32 CAN"));
+
+  if (DESIRED_BIT_RATE < 1000000UL)
+    DIAG(F("CAN id = %d  Bitrate = %d Kb/s  CANH:%d  CANL:%d"), CanMarklin::thisId, DESIRED_BIT_RATE / 100UL, RxPin, TxPin);
+  else
+    DIAG(F("CAN id = %d  Bitrate = %d Mb/s  CANH:%d  CANL:%d"), CanMarklin::thisId, DESIRED_BIT_RATE / 1000000UL, RxPin, TxPin);
+
+  ACAN_ESP32_Settings settings(DESIRED_BIT_RATE);
+  settings.mRxPin = RxPin;
+  settings.mTxPin = TxPin;
+
+  uint32_t errorCode;
+
+  // with filter
+  // const uint32_t inIdentifier = 0x00 << 21;
+  // const uint32_t inDontCareMask = 0x1e1fffff; // Masque pour ne pas prendre en compte les bits 18-25 si > 7
+  // const ACAN_ESP32_Filter filter = ACAN_ESP32_Filter::singleExtendedFilter(
+  //     ACAN_ESP32_Filter::data, inIdentifier, inDontCareMask);
+  // errorCode = ACAN_ESP32::can.begin(DESIRED_BIT_RATE, filter);
+  // Serial.printf("[CanMsg %d] : config with filter\n", __LINE__);
+
+  // without filter
+  errorCode = ACAN_ESP32::can.begin(settings);
+  Serial.printf("[CanMsg %d] : config without filter\n", __LINE__);
+
+  if (errorCode == 0)
+    DIAG(F("CAN Configuration OK !"));
+  else
+  {
+    DIAG(F("CAN Configuration error 0x%x"), errorCode);
+    return;
+  }
+  xQueue = xQueueCreate(50, sizeof(CANMessage));
+  xTaskCreate(ackTask, "ackTask", 2 * 1024, NULL, 0, NULL);
+}
+
+/*--------------------------------------
+  Reception CAN
+  --------------------------------------*/
+
+void CanMarklin::loop()
+{
+  CANMessage frameIn;
+  if (ACAN_ESP32::can.receive(frameIn))
+  {
+    const uint8_t prio = (frameIn.id >> 25) & 0x0F; // Priorité
+    const uint8_t cmde = (frameIn.id >> 17) & 0xFF; // Commande
+    const uint8_t resp = (frameIn.id >> 16) & 0x01; // Reponse
+    const uint16_t exped = (frameIn.id & 0xFFFF);   // Expéditeur
+
+    char cmdName[20];
+    switch (cmde)
+    {
+    case CAN_LOCO_SPEED:
+      strcpy(cmdName, "Loco Speed");
+      break;
+    case CAN_LOCO_DIREC:
+      strcpy(cmdName, "Loco Direction");
+      break;
+    case CAN_LOCO_FUNCT:
+      strcpy(cmdName, "Loco Function");
+      break;
+    case CAN_LOCO_WRITECV_MAIN:
+      strcpy(cmdName, "Loco WriteCv main");
+      break;
+    case CAN_SYSTEM_CONTROL:
+      strcpy(cmdName, "Systeme control");
+      break;
+    }
+
+    if (Diag::CANDIAG)
+      DIAG(F("[CanMarklin]------ Sender %d : Command 0x%0X %s"), exped, cmde, cmdName);
+
+    if (frameIn.rtr) // Remote frame
+      ACAN_ESP32::can.tryToSend(frameIn);
+    else
+    {
+      uint32_t locoAddress = 0;
+      CanMarklinLoco *loco = nullptr;
+      if (cmde > 0x03 && cmde < CAN_FIRST_NOTLOCO_COMMAND || cmde == 0x00 && frameIn.data[4] == 0x03)
+      {
+        static std::vector<CanMarklinLoco> locos;
+        locoAddress = (frameIn.data[0] << 24) | (frameIn.data[1] << 16) | (frameIn.data[2] << 8) | frameIn.data[3];
+        loco = CanMarklinLoco::findLoco(locoAddress);
+      }
+
+      switch (cmde) // Commande appelée
+      {
+      case CAN_LOCO_SPEED:
+        if (loco != nullptr)
+        {
+          if (frameIn.len == 6)
+          {
+            loco->sSpeed((uint16_t)(frameIn.data[4] << 8) | frameIn.data[5]);
+            if (loco->gSpeed() > 1000)
+              loco->sSpeed(1000);
+            uint16_t speed = map(loco->gSpeed(), 0, 1000, 0, 127);
+            if (speed == 1)
+              speed = 0;
+            DCC::setThrottle(loco->gAddress(), speed, loco->gDirection());
+          }
+          else if (frameIn.len == 4) // Demande de la vitesse
+          {
+            frameIn.len = 6;
+            frameIn.data[4] = (loco->gSpeed() & 0xFF00) >> 8;
+            frameIn.data[5] = loco->gSpeed() & 0x00FF;
+          }
+        }
+        xQueueSendToBack(xQueue, &frameIn, 0);
+        break;
+      case CAN_LOCO_DIREC:
+        // Signification du paramètre Direction :
+        // 0 = sens de marche sans changement
+        // 1 = sens de marche avant
+        // 2 = sens de marche arrière
+        // 3 = inverser le sens de marche
+
+        if (loco != nullptr)
+        {
+          uint8_t direction = frameIn.data[4];
+          if (direction)
+          {
+            switch (direction)
+            {
+            case 1:
+              loco->sDirection(1); // Avant
+              break;
+            case 2:
+              loco->sDirection(0); // Arrière
+              break;
+            case 3:
+              loco->sDirection(!loco->gDirection()); // Inversion
+              break;
+            }
+            loco->sSpeed(0);
+            DCC::setThrottle(loco->gAddress(), loco->gSpeed(), loco->gDirection());
+            xQueueSendToBack(xQueue, &frameIn, 0);
+          }
+        }
+        break;
+      case CAN_LOCO_FUNCT:
+        if (loco != nullptr)
+        {
+          DCC::setFn(loco->gAddress(), frameIn.data[4], frameIn.data[5]);
+          Serial.println(loco->gAddress());
+        }
+        break;
+      case CAN_LOCO_WRITECV_MAIN:
+        if (loco != nullptr)
+        {
+          // WRITE CV on MAIN <w CAB CV VALUE>
+          DCC::writeCVByteMain(loco->gAddress(), (frameIn.data[4] << 8) | frameIn.data[5], frameIn.data[6]);
+        }
+        break;
+      case CAN_SYSTEM_CONTROL:
+        uint32_t destId =
+            ((uint32_t)frameIn.data[0] << 24) |
+            ((uint32_t)frameIn.data[1] << 16) |
+            ((uint32_t)frameIn.data[2] << 8) |
+            ((uint32_t)frameIn.data[3]);
+
+        if (destId == thisId) // Verif que la commande s'adresse à cette centrale
+        {
+          switch (frameIn.data[4])
+          {
+          case (0x00):
+          case (0x01):
+            TrackManager::setMainPower(frameIn.data[4] ? POWERMODE::ON : POWERMODE::OFF);
+            xQueueSendToBack(xQueue, &frameIn, 0);
+            break;
+          case (0x02): // emergency stop (all loco)
+            DCC::setThrottle(0, 1, 1);
+            for (auto &[address, loco] : CanMarklinLoco::locoMap) // MAJ de la vitesse pour toutes les locos
+              loco.sSpeed(0);
+            xQueueSendToBack(xQueue, &frameIn, 0);
+            break;
+          }
+        }
+        else if (frameIn.data[4] == 0x03) // emergency stop (only one loco)
+        {
+          if (loco != nullptr)
+          {
+            DCC::setThrottle(loco->gAddress(), 1, loco->gDirection());
+            loco->sSpeed(0);
+            xQueueSendToBack(xQueue, &frameIn, 0);
+          }
+        }
+      }
+    }
+  }
+
+/*--------------------------------------
+  Envoi mesure de courant
+  --------------------------------------*/
+#define MESURE_COURANT
+#ifdef MESURE_COURANT
+  CANMessage frameOut;
+  frameOut.id |= 0x0C << 17;          // commande appelée
+  frameOut.id |= CanMarklin::getId(); // ID expediteur
+  frameOut.ext = true;
+  frameOut.len = 1;
+
+  static uint64_t millisRefreshData = 0;
+  if (millis() - millisRefreshData > 1000)
+  {
+    MotorDriver *mainDriver = NULL;
+    for (const auto &md : TrackManager::getMainDrivers())
+      mainDriver = md;
+    if (mainDriver == NULL || !mainDriver->canMeasureCurrent())
+      return;
+
+    POWERMODE mode = TrackManager::getMainPower();
+    if (mode == POWERMODE::ON)
+    {
+      frameOut.len = 3;
+      uint16_t current = mainDriver->getCurrentRaw();
+      frameOut.data[0] = true;
+      frameOut.data[1] = current >> 8;
+      frameOut.data[2] = current & 0x00FF;
+    }
+    else if (mode == POWERMODE::OVERLOAD)
+      frameOut.data[0] = 2;
+    else
+      frameOut.data[0] = false;
+
+    CanMarklin::sendMsg(frameOut);
+    millisRefreshData = millis();
+  }
+#endif
+}
+
+void CanMarklin::setPower(bool isOn)
+{
+  // CanMarklin::sendMsg(2, CAN_POWERON, CanMarklin::getId(), thisId, 0, isOn);
+}
+
+void CanMarklin::setThrottle(uint16_t cab, uint8_t tSpeed, bool tDirection)
+{
+  if (tSpeed == 1) // Emergency_stop !
+    CanMarklin::emergency();
+  else
+  {
+    // CanMarklin::sendMsg(2, CAN_LOCO_SPEED, CanMarklin::getId(), thisId, 0, (cab & 0xFF00) >> 8, cab & 0x00FF, tSpeed, tDirection);
+  }
+}
+
+void CanMarklin::setFunction(int cab, int16_t functionNumber, bool on)
+{
+  // CanMarklin::sendMsg(2, CAN_LOCO_FUNCTION, CanMarklin::getId(), thisId, 0, (cab & 0xFF00) >> 8, cab & 0x00FF, functionNumber, on);
+}
+
+void CanMarklin::emergency()
+{
+  // CanMarklin::sendMsg(0, CAN_EMERGENCY_STOP, CanMarklin::getId(), thisId, 0);
+}
+
+/************************************************************************
+Retour de LaBox pour les commandes de traction, la fonction emergency stop
+la fonction power on / power off et plus generalement par la suite, toutes
+les fonctions qui emettent une confirmation dans DCC-Ex
+*************************************************************************/
+
+/*--------------------------------------
+  Envoi CAN
+  --------------------------------------*/
+
+void CanMarklin::sendMsg(CANMessage &frame)
+{
+  if (0 == ACAN_ESP32::can.tryToSend(frame))
+    DIAG(F("[CanMarklin]------ Send error"));
+}
+
+#endif
---
+++ CanMarklin.h
@@ -0,0 +1,175 @@
+
+/*
+  CanMsg.h
+
+  Christophe Bobille - Locoduino
+
+  The ESP32 requires to be connected to a CAN transceiver
+*/
+
+#ifndef __CAN_MARKLIN__
+#define __CAN_MARKLIN__
+
+#include <Arduino.h>
+#include <ACAN_ESP32.h>
+#include "DCC.h"
+
+#ifdef ENABLE_CANMARKLIN
+
+#include "TrackManager.h"
+#include "MotorDriver.h"
+#include "EXComm.h"
+#include <unordered_map>
+
+/*
+Les commandes ci-dessous correspondent aux sections pages :
+
+12 et 13 - SYSTEM_CONTROL
+14 - CMD_EMERGENCY
+// Emergncy only one loco : Commande = SYSTEM_CONTROL (0x00) - data[0 -> 4] = add loco - data[4] = 0x03
+// Page 15 de la documentation
+30 - LOCO_SPEED
+31 - LOCO_DIREC
+32 - LOCO_FUNCT
+
+de la documentation 
+https://forum.3rails.fr/uploads/short-url/6cU7HyPkVmnfWErXjLvQgg5Poiz.pdf
+*/
+
+#define CAN_SYSTEM_CONTROL 0x00
+#define CAN_LOCO_SPEED     0x04
+#define CAN_LOCO_DIREC     0x05
+#define CAN_LOCO_FUNCT     0x06
+#define CAN_LOCO_WRITECV_MAIN 0x08
+#define CAN_FIRST_NOTLOCO_COMMAND 0x09
+
+
+#define VERSION_LABOX_CAN "0.7.5"
+
+// 0.7.3  - 12/09/24 : Modification du filtre ACAN_ESP32_Filter
+// 0.7.2  - 07/09/24 : Optimisatin de la recherche findLoco de la classe CanMarklinLoco
+// 0.7.1  - 05/09/24 : Modifications importantes pour les codes de commandes Respect précis du protocole Marklin
+// 0.7.0	- 26/08/24 : Passage à EXComm
+// 0.6.4  - 29/03/24 : Reformattage léger avec remplacement des commandes hexa par des defines.
+//                     utilisation du define CAN pour exclure la compilation de ces sources
+//                     utilisation de DIAG pour les messages à la console.
+// 0.6.3  - 21/02/24 : Modification des identifiants de messages CAN
+// 0.6.2  - 12/12/23 : Ajout d'une méthode pour acquisition de hash (supprimé ensuite)
+//                     La declaration des broches du CAN et de la vitesse ont ete deplcees lignes 184 à 186 de config.h
+// 0.6.1  - 11/12/23 : Ajout du retour d'informations
+//                     Ajout de commandes dont la POM  case 0xF7:
+//                       WRITE CV on MAIN <w CAB CV VALUE>
+// 0.6.0  - 11/12/23 : Adoption d'un nouveau format de messages totalement incompatible avec les anciens
+// 0.5.5  - 11/12/23 : Correction inversion :
+//                     case 0xFE:
+//                     TrackManager::setMainPower(frameIn.data[0] ? POWERMODE::OFF : POWERMODE::ON);
+// 0.5.4  - 10/12/23 : Add POWERMODE::OVERLOAD
+// 0.5.3  - 10/12/23
+// 0.5.2  - 09/12/23
+// 0.5.1  - 09/12/23 : Fix oubli break  case 0xFE:
+//                     TrackManager::setMainPower(frameIn.data[0] ? POWERMODE::ON : POWERMODE::OFF);
+//                     break;
+// 0.5    - 09/12/23 : Ajout du retour d'information au programme de test. C'est la mesure de courant qui a ete choisie
+//                     pour cela lignes 83 a 97 du programme.
+// 0.4    - 08/12/23
+
+class CanMarklinLoco
+{
+private:
+  uint32_t address;
+  uint16_t speed;
+  bool direction;
+
+public:
+  // Constructeur par défaut
+  CanMarklinLoco() : address(0), speed(0), direction(1) {}
+  // Constructeur avec paramètre adresse
+  CanMarklinLoco(uint32_t addr) : address(addr), direction(1) {}
+
+  void sAddress(uint32_t address) { this->address = address; }
+  void sSpeed(uint16_t speed) { this->speed = speed; }
+  void sDirection(uint8_t direction) { this->direction = direction; }
+  uint32_t gAddress() const { return address; }
+  uint16_t gSpeed() const { return speed; }
+  uint8_t gDirection() const { return direction; }
+
+  static std::unordered_map<uint32_t, CanMarklinLoco> locoMap;
+  static CanMarklinLoco *findLoco(uint32_t address);
+};
+
+class CanMarklin : public EXCommItem
+{
+private:
+  static gpio_num_t RxPin;
+  static gpio_num_t TxPin;
+  static uint32_t DESIRED_BIT_RATE;
+  static uint16_t thisId;
+  static bool SendCommands;
+
+public:
+  // EXCommItem part
+  CanMarklin(uint16_t id, gpio_num_t rxPin, gpio_num_t txPin, uint32_t bitRate, bool sendCommands) : EXCommItem("CAN")
+  {
+    thisId = id;
+    RxPin = rxPin;
+    TxPin = txPin;
+    DESIRED_BIT_RATE = bitRate;
+    SendCommands = sendCommands;
+    this->MainTrackEnabled = true;
+    this->ProgTrackEnabled = true;
+    this->AlwaysLoop = false;
+  }
+
+  bool beginItem()
+  {
+    begin();
+    return true;
+  }
+  
+  bool loopItem()
+  {
+    loop();
+    return true;
+  }
+
+  void sendPower(bool isOn)
+  {
+    if (SendCommands)
+      setPower(isOn);
+  }
+
+  void sendThrottle(uint16_t cab, uint8_t tSpeed, bool tDirection)
+  {
+    if (SendCommands)
+      setThrottle(cab, tSpeed, tDirection);
+  }
+
+  void sendFunction(int cab, int16_t functionNumber, bool on)
+  {
+    if (SendCommands)
+      setFunction(cab, functionNumber, on);
+  }
+  void sendEmergency()
+  {
+    if (SendCommands)
+      emergency();
+  }
+  // End EXCommItem
+
+  static void begin();
+  static void loop();
+  static uint16_t getId();
+
+  static void setPower(bool isOn);
+  static void setThrottle(uint16_t cab, uint8_t tSpeed, bool tDirection);
+
+  // static void setSpeed(uint16_t cab, uint8_t tSpeed);
+  // static void setDirection(uint16_t cab, bool tDirection);
+  static void setFunction(int cab, int16_t functionNumber, bool on);
+  static void emergency();
+
+  static void sendMsg(CANMessage &);
+};
+
+#endif
+#endif
